6c279c0d3f715429a92f28e6ca05e86b3d2f7e14
Charly Escalona <charly2221@hotmail.fr>
2021-03-12 16:49:46 +0100

    added dist

---
+++ dist/serviceWorker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("pwa").then((cache) => {
+      return cache
+        .addAll(["/", ".serviceWorker.js", "index.html"])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.responseWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
