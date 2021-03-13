4da5739b27f197fa9182bd169bad5aebb0bff3cc
Charly Escalona <charly2221@hotmail.fr>
2021-03-13 13:00:24 +0100

    Ajout des vues des premières vues

---
+++ src/views/AddPicture.vue
@@ -0,0 +1,32 @@
+<template>
+    <form-add>
+        <input-form>
+            <label for="">Nom </label>
+            <input type="text" @change="uploadImage($event)" id="text-input" name="resource" :placeholder="placeholder">
+        </input-form>
+        <input-form>
+            <label for="">Chemin vers la resource </label>
+            <input type="text" @change="uploadImage($event)" id="text-input" name="resource" :placeholder="placeholder">
+        </input-form>
+        <button-form>Poster</button-form>
+    </form-add>
+</template>
+
+<script>
+    import FormAdd from '../components/FormAdd'
+    import InputForm from '../components/elements/InputForm'
+    import ButtonForm from '../components/elements/ButtonForm'
+    export default {
+        name: 'AddPicture',
+        components: {
+            FormAdd,
+            InputForm,
+            ButtonForm
+        }
+    }
+</script>
+
+<style lang="scss">
+
+
+</style>
---
+++ src/views/AddTag.vue
@@ -0,0 +1,28 @@
+<template>
+    <form-add>
+        <input-form>
+            <label for="">Nom </label>
+            <input type="text" @change="uploadImage($event)" id="text-input" name="resource" :placeholder="placeholder">
+        </input-form>
+        <button-form>Ajouter ce tag</button-form>
+    </form-add>
+</template>
+
+<script>
+    import FormAdd from '../components/FormAdd'
+    import InputForm from '../components/elements/InputForm'
+    import ButtonForm from '../components/elements/ButtonForm'
+    export default {
+        name: 'AddPicture',
+        components: {
+            FormAdd,
+            InputForm,
+            ButtonForm
+        }
+    }
+</script>
+
+<style lang="scss">
+
+
+</style>
