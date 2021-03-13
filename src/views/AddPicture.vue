e798c46be28497a1274e81f2f13df5e478f3dfd6
Charly Escalona <charly2221@hotmail.fr>
2021-03-13 13:24:21 +0100

    Ajout d'un composant select à une vue

--- src/views/AddPicture.vue
+++ src/views/AddPicture.vue
@@ -8,6 +8,14 @@
             <label for="">Chemin vers la resource </label>
             <input type="text" @change="uploadImage($event)" id="text-input" name="resource" :placeholder="placeholder">
         </input-form>
+        <select-form>
+            <option>fsdfsdfsdfsd</option>
+            <option>fsdfsdfsdfsd</option>
+            <option>fsdfsdfsdfsd</option>
+            <option>fsdfsdfsdfsd</option>
+            <option>fsdfsdfsdfsd</option>
+        </select-form>
+
         <button-form>Poster</button-form>
     </form-add>
 </template>
@@ -15,12 +23,14 @@
 <script>
     import FormAdd from '../components/FormAdd'
     import InputForm from '../components/elements/InputForm'
+    import SelectForm from '../components/elements/SelectForm'
     import ButtonForm from '../components/elements/ButtonForm'
     export default {
         name: 'AddPicture',
         components: {
             FormAdd,
             InputForm,
+            SelectForm,
             ButtonForm
         }
     }
